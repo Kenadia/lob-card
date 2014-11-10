861565d66e45f76df4faff6110ac24596df5d622
Ken Schiller <kenschiller@gmail.com>
2014-11-10 15:23:14 -0600

    Add a command-line interface with basic errors

--- lob_card.py
+++ lob_card.py
@@ -4,7 +4,7 @@ import config
 lob.api_key = config.API_KEY
 
 
-def verify_params(addressee_name, file_path, message):
+def errors_for_params(addressee_name, file_path, message):
     errors = []
     message_length = len(message)
     if message_length < 1 or message_length > 350:
@@ -15,33 +15,47 @@ def verify_params(addressee_name, file_path, message):
 def create_card(addressee_name, file_path, message):
     errors = errors_for_params(addressee_name, file_path, message)
     if errors:
-        lob.Postcard.create(
-            to_address = {
-                'name': addressee_name,
-                'address_line1': '300 N College St',
-                'address_city': 'Northfield',
-                'address_state': 'MN',
-                'address_zip': '55057',
-                'address_country': 'US'
-            },
-            from_address = {
-                'name': 'LobCard',
-                'address_line1': '300 N College St',
-                'address_city': 'Northfield',
-                'address_state': 'MN',
-                'address_zip': '55057',
-                'address_country': 'US'
-            },
-            front = open(file_path, 'rb'),
-            message = message
-        )
+        # Todo: Throw a custom error
         return False
-    else:
-        return True
+    return lob.Postcard.create(
+        to_address = {
+            'name': addressee_name,
+            'address_line1': '300 N College St',
+            'address_city': 'Northfield',
+            'address_state': 'MN',
+            'address_zip': '55057',
+            'address_country': 'US'
+        },
+        from_address = {
+            'name': 'LobCard',
+            'address_line1': '300 N College St',
+            'address_city': 'Northfield',
+            'address_state': 'MN',
+            'address_zip': '55057',
+            'address_country': 'US'
+        },
+        front = open(file_path, 'rb'),
+        message = message,
+        full_bleed = 1,
+    )
 
 
 def main():
-    create_card("Ken Schiller", "/test.pdf", "Hi! This is cool!");
+    print 'Name of recipient:',
+    name = raw_input().strip()
+    print 'Local path to image to use for postcard front (must be 6.25in x 4.25in):',
+    filename = raw_input().strip()
+    print 'Message:',
+    message = raw_input().strip()
+    try:
+        if create_card(name, filename, message):
+            print 'Success! Your postcard has been sent.'
+        else:
+            print 'Error: Invalid parameters'
+    except IOError:
+        print 'Error: The specified image was not found.'
+    except InvalidRequestError:
+        print 'Error: Invalid request.'
 
 
 if __name__ == '__main__':
